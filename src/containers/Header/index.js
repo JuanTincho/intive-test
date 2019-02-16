215d268734becd0df98de791e7fa238a1e23de3c
Juan Martín Pithod <juantincho@gmail.com>
2019-02-16 20:39:34 -0300

    Add state to Header

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Grid, withStyles, MenuItem } from '@material-ui/core';
+import { Button, Grid, MenuItem, withStyles } from '@material-ui/core';
 import TextField from '../../components/TextField';
 
 const styles = {
@@ -9,26 +9,66 @@ const styles = {
   }
 };
 
-const Header = ({ classes }) => {
-  return (
-    <Grid container className={classes.header} alignItems="center" spacing={24}>
-      <TextField placeholder="Player Name" />
-      <TextField placeholder="Position" select value="Position">
-        <MenuItem value="Position">Position</MenuItem>
-      </TextField>
-      <TextField
-        placeholder="Age"
-        type="number"
-        InputProps={{ inputProps: { min: 18, max: 40 } }}
-      />
-      <Grid item xs={3}>
-        <Button color="primary" variant="contained">
-          Search
-        </Button>
+class Header extends Component {
+  state = {
+    age: null,
+    name: '',
+    position: ''
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  searchPlayers = () => {};
+
+  render() {
+    const { age, name, position } = this.state;
+    const { classes } = this.props;
+    return (
+      <Grid
+        container
+        className={classes.header}
+        alignItems="center"
+        spacing={24}
+      >
+        <TextField
+          placeholder="Player Name"
+          value={name}
+          name="name"
+          onChange={this.handleChange}
+        />
+        <TextField
+          placeholder="Position"
+          select
+          value={position}
+          name="position"
+          onChange={this.handleChange}
+        >
+          <MenuItem value="Position">Position</MenuItem>
+        </TextField>
+        <TextField
+          placeholder="Age"
+          type="number"
+          name="age"
+          value={age}
+          onChange={this.handleChange}
+          InputProps={{ inputProps: { min: 18, max: 40 } }}
+        />
+        <Grid item xs={3}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.searchPlayers}
+          >
+            Search
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
-  );
-};
+    );
+  }
+}
 
 Header.propTypes = {};
 
