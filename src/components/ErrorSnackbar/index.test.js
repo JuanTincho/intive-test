72fe273eed75fdbc73861223a1d0378d679a6f1d
Juan Martín Pithod <juantincho@gmail.com>
2019-02-20 22:07:58 -0300

    Add test to snackbar and configure Coverage

--- src/components/ErrorSnackbar/index.test.js
+++ src/components/ErrorSnackbar/index.test.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Button, Snackbar } from '@material-ui/core';
+import { Snackbar } from '@material-ui/core';
+import { createMount } from '@material-ui/core/test-utils';
+import { act } from 'react-dom/test-utils';
 import { ErrorSnackbar } from './index';
 
 describe('ErrorSnacknar', () => {
   let errorSnackbarWrapper;
   let fetchData;
-
-  const snackbarRoot = global.document.createElement('div');
-  snackbarRoot.setAttribute('id', 'snackbar-root');
-  const body = global.document.querySelector('body');
-  body.appendChild(snackbarRoot);
+  let buttons;
+  let snackbarComponent;
+  let mount;
 
   beforeEach(() => {
+    mount = createMount();
     fetchData = jest.fn();
-    errorSnackbarWrapper = shallow(<ErrorSnackbar fetchData={fetchData} error />);
+    errorSnackbarWrapper = mount(<ErrorSnackbar fetchData={fetchData} error />);
+
+    snackbarComponent = errorSnackbarWrapper.find(Snackbar);
+    buttons = snackbarComponent.prop('action');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    mount.cleanUp();
   });
 
   it('should open when there is an error', () => {
-    expect(errorSnackbarWrapper.find(Snackbar).prop('open')).toBeTruthy();
+    expect(snackbarComponent.prop('open')).toBeTruthy();
   });
 
-  it('should fetch the players data when Retry is clicked', () => {
-    errorSnackbarWrapper
-      .dive()
-      .shallow()
-      .find(Button)
-      .simulate('click');
-    // expect(button).toEqual({ hola: '2' });
+  it('should fetch the players data when Retry button is clicked', () => {
+    expect(buttons[0].props.id).toEqual('button-retry');
+    act(() => {
+      buttons[0].props.onClick();
+    });
     expect(fetchData).toHaveBeenCalledTimes(1);
+  });
+
+  it('should close the Snackbar when Close button is clicked', () => {
+    expect(buttons[1].props.id).toEqual('button-close');
+    act(() => {
+      buttons[1].props.onClick();
+    });
+    errorSnackbarWrapper.update();
+    expect(errorSnackbarWrapper.find(Snackbar).prop('open')).toBeFalsy();
   });
 });
